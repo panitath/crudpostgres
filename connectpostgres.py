d764e30de6d0260135d454ef712b8c0243f77fc2
Panita <panita.tharaphom@gmail.com>
2023-09-21 11:56:05 +0700

    dd

---
+++ connectpostgres.py
@@ -0,0 +1,22 @@
+import pymysql
+# สร้าง function เชื่อ
+
+
+def connectdb():
+    # connection = pymysql.connect(
+    #     host='localhost',
+    #     user='root',
+    #     password='',
+    #     db='pythondb',
+    #     port=3306,
+    #     cursorclass=pymysql.cursors.DictCursor
+
+    connection = pymysql.connect(
+        host='dpg-ck5snrr6fquc739cqou0-a',
+        user='admin',
+        password='u0r0E8RpBxTX9aWEcdcy3gvLDWwog3uQ',
+        database='sampledb_9jcz',
+        port=3306,
+       
+    )
+    return connection
